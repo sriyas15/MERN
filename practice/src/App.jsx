d1813d4548eabb9686eee1796bf7e6d8ea5b62fb
Mohamed Riyas <sriyas1508@gmail.com>
2025-11-18 14:03:37 +0530

    setting up

---
+++ practice/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div data-theme="forest">
+      <h1 className='text-2xl font-bold'>Mohamed</h1>
+      <button className='btn btn-primary'>Add</button>
+    </div>
+  )
+}
+
+export default App
