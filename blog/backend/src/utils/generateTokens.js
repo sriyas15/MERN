474e7b0edba6df65143ec5e565c2a8e9d76813d9
Mohamed Riyas <sriyas1508@gmail.com>
2026-05-24 11:53:37 +0530

    backend host setup

--- blog/backend/src/utils/generateTokens.js
+++ blog/backend/src/utils/generateTokens.js
@@ -1,18 +1,23 @@
 import jwt from "jsonwebtoken";
 
-export const generateTokens = (res,userId)=>{
+export const generateTokens = (res, userId) => {
 
-    const token = jwt.sign({_id:userId},process.env.JWT_SECRET_KEY,{
-        expiresIn:"30d"
+    const token = jwt.sign(
+        { _id: userId },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: "30d"
+        }
+    );
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production"
+            ? "none"
+            : "lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
-    res.cookie("jwt",token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "production",
-        sameSite:"strict",
-        maxAge: 30*24*60*60*1000
-    });
-
-    return token
-}
-
+    return token;
+};
